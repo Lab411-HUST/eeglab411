e7f08b947163e807f43f5f1c304ab65283624076
tienku <tienbk92@gmail.com>
2015-05-09 09:15:43 +0700

    Update OpenVIBE for Android

--- KeyBoard/OpenVibeForAndroid/jni/Application.mk
+++
@@ -1,8 +0,0 @@
-
-# Uncomment this if you're using STL in your project
-# See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
-# APP_STL := stlport_static 
-APP_STL := gnustl_static
-APP_CPPFLAGS := -frtti -fexceptions
-APP_ABI := armeabi-v7a armeabi x86
-APP_PLATFORM := android-8
--- KeyBoard/OpenVibeForAndroid/src/com/example/processSignal/SetupCommon.java
+++
@@ -1,7 +0,0 @@
-package com.example.processSignal;
-
-public class SetupCommon {
-	public static int NUMBER_CHANEL=14;
-	public static int NUMBER_SAMPLE_PER_CHANEL=32;
-	
-}
